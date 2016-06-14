fb270699b9c64e5643b5b4b280ce820128425f92
Alexander <wlkalexander@gmail.com>
2016-06-14 13:38:56 +0200

    Delete setVars.bat
    del srvbat

--- webgui/setVars.bat
+++
@@ -1,6 +0,0 @@
-set adname=aditosoftware.local
-set baseDN=dc=aditosoftware,dc=local
-set showUser=icinga2
-set showPass=I2ci5naga2a2015
-set adminGroup=IT,Test,Schema-Admins
-set loginGroup=ADTest,ADTest2
